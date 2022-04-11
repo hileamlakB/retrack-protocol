ae3e568a99fbac6fd2ec10487a39ba2372ceed86
Hileamlak Yitayew <hileamlak@localhost.localdomain>
2022-04-10 23:17:51 -0700

    Add more events

--- contracts/RetrackProtocol.sol
+++ contracts/RetrackProtocol.sol
@@ -7,7 +7,7 @@ import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.so
 
 // TODO
 // Expand this protocol for other tokens as well on top of native currency 
-
+// Add grace period even before the reedeming period
 
 contract RetrackProtocol is ERC20 {
     
@@ -20,8 +20,10 @@ contract RetrackProtocol is ERC20 {
     mapping(address => mapping (address => expiringAmount)) private recipients;
 
 
-    event MoneyTransferred (address from, address to, uint256 amount);
-    event Moneyredeemed (address from, address redeemer, uint256 amount);
+    event MoneyTransferred (address indexed from, address indexed to, uint256 amount);
+    event MoneyRedeemed (address indexed from, address indexed redeemer, uint256 amount);
+    event MoneyRetracked (address indexed from, address indexed by, uint256 amount);
+    event RightTransferred (address indexed from, address indexed to, address indexed from_inital, uint256 ammount);
 
 
     constructor() ERC20("RetrackProtocol", "REPRO") {
@@ -36,7 +38,6 @@ contract RetrackProtocol is ERC20 {
     /// @param redeeming_period Period during which transfered could be redeemed, during this period
     ///                 senders can't retrack, the unit is in days
     ///                 the redeeming_period exists to prevent concquerency attack where sender
-    ///                 and redeemer try to access the same fund at the same time
     function send (address account, uint redeeming_period) public payable {
         require(msg.value > 0, "No money deposited");
         // This is to make sure no typo error happens for the redeeming period
@@ -53,8 +54,7 @@ contract RetrackProtocol is ERC20 {
     ///      which one to redeem from 
     /// @param from sender to redeem from 
     /// @param redeem_amount amount to redeem
-    /// @return status true on sucess
-    function redeem (address from, uint256 redeem_amount) public returns(bool){
+    function redeem (address from, uint256 redeem_amount) public {
         
         require(redeem_amount >= 0, "You don't have anything to withdraw");
         require(recipients[msg.sender][from].amount >= redeem_amount, "You don't have this much redeem_amount from provided address");
@@ -65,7 +65,7 @@ contract RetrackProtocol is ERC20 {
         _burn(msg.sender, redeem_amount);
         recipients[msg.sender][from].amount = recipients[msg.sender][from].amount - redeem_amount;
         payable(msg.sender).transfer(redeem_amount);
-        return true;
+        emit MoneyRedeemed(from, msg.sender, redeem_amount);
             
     }
 
@@ -75,8 +75,7 @@ contract RetrackProtocol is ERC20 {
     /// @param to new reciever address
     /// @param amount amount to be transfered
     /// @param redeeming_period new redeeming_period starting from transfering time
-    /// @return status returns true on sucess
-    function transferRight(address from, address to, uint256 amount, uint redeeming_period) public returns (bool){
+    function transferRight(address from, address to, uint256 amount, uint redeeming_period) public{
 
         require(recipients[msg.sender][from].amount >= amount, "You don't have this much amount from provided address");
         require(balanceOf(msg.sender) >= amount, "You should have equivalent number of coins");
@@ -86,7 +85,7 @@ contract RetrackProtocol is ERC20 {
         _transfer(msg.sender, to, amount);
         recipients[msg.sender][from].amount = recipients[msg.sender][from].amount - amount;
         recipients[to][msg.sender] = expiringAmount(recipients[to][msg.sender].amount + amount, block.timestamp + redeeming_period * 86400);
-        return true;
+        emit RightTransferred(msg.sender, to, from, amount);
 
     }
 
@@ -95,8 +94,7 @@ contract RetrackProtocol is ERC20 {
     /// @dev EAs explained above redeeming period is inplace to prevent race conditions
     /// @param from recievers addres to pool back from
     /// @param amount amount to pull back
-    /// @return status true on success
-    function retrack (address from, uint256 amount) public returns (bool){
+    function retrack (address from, uint256 amount) public{
 
 
         require(recipients[from][msg.sender].amount >= amount, "Address doesn't owe you this much! May be amount is redeemed");
@@ -109,7 +107,7 @@ contract RetrackProtocol is ERC20 {
         recipients[from][msg.sender].amount =  recipients[from][msg.sender].amount - amount;
         // send back deposited money
         payable(msg.sender).transfer(amount);
-        return true;
+        emit MoneyRetracked(from, msg.sender, amount);
        
     }
 
